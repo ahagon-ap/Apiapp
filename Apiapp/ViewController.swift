61316eb0b18ca3366e70cf7e7de0f827b1326490
Ahagon-ap <s.ahagon@alphapolis.co.jp>
2024-07-16 15:07:39 +0900

    Initial Commit

---
+++ Apiapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apiapp
+//
+//  Created by WEBSYSTEM-MAC41 on 2024/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
