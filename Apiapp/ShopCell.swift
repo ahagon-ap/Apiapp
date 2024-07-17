af16de1514602c2cda0f50f45ece436bc880ed89
Ahagon-ap <s.ahagon@alphapolis.co.jp>
2024-07-17 13:32:42 +0900

    入力欄の追加

---
+++ Apiapp/ShopCell.swift
@@ -0,0 +1,16 @@
+//
+//  ShopCell.swift
+//  Apiapp
+//
+//  Created by WEBSYSTEM-MAC41 on 2024/07/16.
+//
+
+import UIKit
+
+class ShopCell: UITableViewCell {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var shopNameLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+
+}
